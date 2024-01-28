2df45e49bf9f94f01cb71804f638652476292f42
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-01-28 22:13:15 +0530

    Create Code.py

---
+++ Others/Date-time Module/Code.py
@@ -0,0 +1,10 @@
+from datetime import *
+nowh = datetime.now()
+print(nowh)
+now = date.today()
+print(now)
+print(now.day)
+nowl = datetime(year = 2022 , month = 9, day = 24 , hour = 12, minute = 30, second = 0)
+print(nowl)
+print(nowl.day)
+print(nowh - nowl)
