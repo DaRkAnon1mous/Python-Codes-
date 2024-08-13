b77efcec00268f7aa0f15b199c8c76960d72c031
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-08-13 21:52:41 +0530

    Create Code.py

---
+++ Hackerrank/Problem Solving/Diagonal Difference/Code.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    sumi = 0
+    suma = 0 
+    m = len(arr)
+    for i in range(m):
+        sumi += arr[i][i]  # Primary diagonal: elements where row index equals column index
+        suma += arr[i][m-i-1]
+    c = abs(sumi - suma)
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
