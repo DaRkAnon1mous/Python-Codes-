3f217cdcd98ede9e3b1d87f78546f3bcaa5a20f6
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-11-16 21:17:29 +0530

    Create Code.py

---
+++ Hackerrank/Python Problems/What's Your Name?/Code.py
@@ -0,0 +1,20 @@
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    str1 = first
+    str2 = last
+    result = "Hello "+str1+" "+str2+"!"+" You just delved into python."
+    print( result)
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
