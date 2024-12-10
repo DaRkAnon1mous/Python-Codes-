8bbf9480df9efa7e03b4665a1e3d9207ae1969cf
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-12-10 19:12:25 +0530

    Created Code.py 10-12-24

---
+++ Hackerrank/Problem Solving/Migratory Birds/Code.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import statistics
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    m = statistics.multimode(arr)
+    return min(m)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
