bfc73ffd54bb7be3bff0693f60fb07b57406f519
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-12-25 00:03:57 +0530

    Updated Code.py

--- Hackerrank/Problem Solving/Bill Division/Code.py
+++ Hackerrank/Problem Solving/Bill Division/Code.py
@@ -1,1 +1,40 @@
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    anna_share = (sum(bill) - bill[k]) // 2
+
+    if b == anna_share:
+        print("Bon Appetit")
+    else:
+        print(b - anna_share)
+    
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
