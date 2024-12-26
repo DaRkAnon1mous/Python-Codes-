2fb9d773d67911c407bb5390f6f32ac322cff75e
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-12-26 23:53:23 +0530

    Create Code.py

---
+++ Hackerrank/Problem Solving/Drawing Book/Code.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    # Write your code here
+    front_turns = p // 2
+    
+    # Turns from the back
+    back_turns = (n // 2) - (p // 2)
+    
+    # Return the minimum of the two
+    return min(front_turns, back_turns)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
