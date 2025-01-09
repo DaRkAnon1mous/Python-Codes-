42097cb65f6a113a177815c39cef3ef12511e46c
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2025-01-09 22:21:28 +0530

    Create Code.py

---
+++ Hackerrank/Problem Solving/Forming a Magic Square/Code.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def formingMagicSquare(s):
+    # Write your code here
+    magic_squares = [
+        [8, 1, 6, 3, 5, 7, 4, 9, 2],
+        [6, 1, 8, 7, 5, 3, 2, 9, 4],
+        [4, 9, 2, 3, 5, 7, 8, 1, 6],
+        [2, 9, 4, 7, 5, 3, 6, 1, 8],
+        [8, 3, 4, 1, 5, 9, 6, 7, 2],
+        [4, 3, 8, 9, 5, 1, 2, 7, 6],
+        [6, 7, 2, 1, 5, 9, 8, 3, 4],
+        [2, 7, 6, 9, 5, 1, 4, 3, 8],
+    ]
+    
+    # Flatten the input 3x3 matrix
+    s_flat = sum(s, [])
+    
+    # Calculate the minimal cost to convert `s` into a magic square
+    min_cost = float('inf')
+    for magic in magic_squares:
+        cost = sum(abs(s_flat[i] - magic[i]) for i in range(9))
+        min_cost = min(min_cost, cost)
+    
+    return min_cost
+
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = []
+
+    for _ in range(3):
+        s.append(list(map(int, input().rstrip().split())))
+
+    result = formingMagicSquare(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
