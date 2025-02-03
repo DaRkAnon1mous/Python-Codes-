e52fc7e504f5123bd66b1fa76ffaa7a35e82452f
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2025-02-04 01:35:08 +0530

    Create Code1.py

---
+++ Leetcode/N-th Tribonacci Number/Code1.py
@@ -0,0 +1,9 @@
+## Recursive Approach Time Limit Exceed 
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n==2:
+            return 1
+        else:
+            return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
