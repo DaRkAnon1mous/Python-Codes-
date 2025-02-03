cbac5c5d2d1fb7dd335860f0b24d3efbcdc1b5d0
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2025-02-04 01:41:29 +0530

    Create Code2.py

---
+++ Leetcode/N-th Tribonacci Number/Code2.py
@@ -0,0 +1,9 @@
+## Memoization Approach:
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        arr= {0:0,1:1,2:1}
+        if n in arr:
+            return arr[n]
+        else:
+            arr[n]=self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
+            return arr[n]
