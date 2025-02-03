4edee8b85aca6dc69a6213d866ebec391fb5312f
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2025-02-04 01:55:48 +0530

    Create Code3.py

---
+++ Leetcode/N-th Tribonacci Number/Code3.py
@@ -0,0 +1,17 @@
+## Tabulation Approach T=O(n) S=O(1) best optimised
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        f0 = 0
+        f1 = 1
+        f2 = 1
+        curr =0
+        if n == 0:
+            return f0
+        if n==1 or n==2:
+            return f1
+        for i in range(3,n+1):
+            curr = f0+f1+f2
+            f0=f1
+            f1 =f2
+            f2 =curr
+        return curr
